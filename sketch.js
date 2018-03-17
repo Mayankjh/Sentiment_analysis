5436a5348021feb100764cca5654460c7d23a6f8
Mayank Jha <mayankjha7722@gmail.com>
2018-03-17 23:16:48 +0530

    minor

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var table;
 
 function preload(){
-  table = loadTable('AFINN-111.txt');
+  table = loadTable('AFINN-111.txt', 'tsv');
 }
 
 
@@ -9,8 +9,13 @@ function setup() {
   noCanvas();
   console.log(table);
   console.log('sentiment');
+  for (var i=0; i< table.getRowCount(); i++){
+    var row = table.getRow(i);
+    var word = row.get(0);
+    var score = row.get(1);
+    console.log(row);
+ }
 }
-
 function draw(){
 
 }
